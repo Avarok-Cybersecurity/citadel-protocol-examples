267489071192e7f94a0b8f8b6f42b730b9c981d1
Tjemmmic <mdtjemmes@proton.me>
2024-01-24 12:47:50 -0600

    c2s example

--- internal-service-example/src/bin/client.rs
+++ internal-service-example/src/bin/client.rs
@@ -4,16 +4,61 @@ use citadel_proto::prelude::{
 };
 use citadel_sdk::prefabs::client::single_connection::SingleClientServerConnectionKernel;
 use citadel_sdk::prelude::NodeBuilder;
-use futures::StreamExt;
+use futures::{SinkExt, StreamExt};
 use std::net::SocketAddr;
 use std::str::FromStr;
 use uuid::Uuid;
 use citadel_internal_service::*;
 use citadel_internal_service::kernel::*;
+use citadel_internal_service_connector::util::*;
+use citadel_internal_service_types::*;
+use tokio::net::TcpStream;
 
 #[tokio::main]
 async fn main() {
-    // let (server, server_bind_address) = server_info_skip_cert_verification();
-    // tokio::task::spawn(server);
+    // Connect to Internal Service via TCP
+    let bind_address_internal_service: SocketAddr = "127.0.0.1:23457".parse().unwrap();
+    let tcp_conn = TcpStream::connect(bind_address_internal_service).await.unwrap();
+    let (mut sink, mut stream) = wrap_tcp_conn(tcp_conn).split();
+
+    // Receive Greeter Packet
+    let first_packet = stream.next().await.unwrap().unwrap();
+    let greeter_packet: InternalServiceResponse = bincode2::deserialize(&first_packet).unwrap();
+    if let InternalServiceResponse::ServiceConnectionAccepted(ServiceConnectionAccepted) = greeter_packet
+    {
+        println!("Client Successfully Connected To Internal Service");
+    } else {
+        panic!("Error occurred while trying to connect to Internal Service");
+    }
+
+    // Register To and Connect To Server
+    let session_security_settings = SessionSecuritySettingsBuilder::default()
+        .build()
+        .unwrap();
+    let register_request = InternalServiceRequest::Register {
+        request_id: Uuid::new_v4(),
+        server_addr: "127.0.0.1:23458".parse().unwrap(),
+        full_name: "Client One".parse().unwrap(),
+        username: "ClientOne".parse().unwrap(),
+        proposed_password: "secret".into(),
+        session_security_settings,
+        connect_after_register: true,
+    };
+    sink.send(register_request.into()).await.unwrap();
+    let serialized_response = stream.next().await.unwrap().unwrap();
+    let register_response = deserialize(&serialized_response).unwrap();
+    match register_response {
+        InternalServiceResponse::RegisterSuccess( RegisterSuccess { request_id: _ } ) => {
+            println!("Client Successfully Registered and Connected to Server")
+        },
+
+        InternalServiceResponse::RegisterFailure( RegisterFailure { message, request_id: _ } ) => {
+            println!("Client Failed to Register/Connect to Server: {message:?}")
+        },
+
+        _ => {
+            panic!("Unhandled Response While Trying to Register/Connect to Server")
+        },
+    }
 
 }
--- internal-service-example/src/bin/server.rs
+++ internal-service-example/src/bin/server.rs
@@ -3,22 +3,46 @@ use citadel_sdk::prefabs::server::client_connect_listener::ClientConnectListener
 use citadel_sdk::prelude::*;
 use futures::StreamExt;
 use std::net::SocketAddr;
+use std::path::PathBuf;
 use std::str::FromStr;
+use std::sync::Arc;
+use std::sync::atomic::AtomicBool;
 use citadel_internal_service::*;
 use citadel_internal_service::kernel::*;
+use citadel_sdk::prefabs::ClientServerRemote;
+use futures::future::join_all;
 
 #[tokio::main]
 async fn main() {
-    let bind_address_internal_service: SocketAddr = "127.0.0.1:23456".parse().unwrap();
+    // Server
+    let tcp_listener = std::net::TcpListener::bind("127.0.0.1:23458").unwrap();
+    let bind_addr = tcp_listener.local_addr().unwrap();
+    let kernel = Box::new(ClientConnectListenerKernel::new(
+        move |connect_success, remote| async move {
+            let client_cid = connect_success.cid;
+            println!("Hello World! From Client {client_cid:?}");
+            Ok(())
+        }
+    ));// as Box<dyn NetKernel>;
+    let mut builder = NodeBuilder::default();
+    let builder = builder
+        .with_node_type(NodeType::Server(bind_addr))
+        .with_insecure_skip_cert_verification()
+        .with_underlying_protocol(
+            ServerUnderlyingProtocol::from_tcp_listener(tcp_listener).unwrap(),
+        );
+
+    let server = builder.build(kernel).unwrap();
+    tokio::task::spawn(server);
+
+    // Internal Service
+    let bind_address_internal_service: SocketAddr = "127.0.0.1:23457".parse().unwrap();
     let internal_service_kernel = CitadelWorkspaceService::new(bind_address_internal_service);
     let internal_service = NodeBuilder::default()
         .with_node_type(NodeType::Peer)
         .with_backend(BackendType::InMemory)
         .with_insecure_skip_cert_verification()
-        .build(internal_service_kernel)?;
+        .build(internal_service_kernel).unwrap();
+    tokio::task::spawn(internal_service);
 
-    match internal_service.await {
-        Ok(_) => { std::println!("Success") }
-        Err(err) => { std::println!("Error: {:?}", err) }
-    }
 }
